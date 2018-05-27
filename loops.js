319d88752127a9b912b791b324dcc65a8d40dc2a
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:05:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,12 +3,17 @@
 var array = [];
 
 function forLoop(arr) {
+  //add string to array 25 times
   for(let i = 0; i < 25; i++) {
+    //if i value is 1, add 'I am 1 strange loop'
     if(i === 1){
-      arr.push(`I am ${i} strange loop.`);
-    } else {
+      arr.push(`I am 1 strange loop.`);
+    } 
+    //otherwise, add 'I am ${i} strange loops'
+    else {
       arr.push(`I am ${i} strange loops.`);
     }
   }
+  //return array
   return arr;
 }
