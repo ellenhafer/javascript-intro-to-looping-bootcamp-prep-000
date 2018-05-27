efa6ae1635813ccc4ae30a1e37620faee72e346c
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:02:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,7 +5,12 @@
 // Use a for loop when you know how many times you want the loop to run (for example, when you have an array of known size).
 
 function forLoop(arr) {
-  for(var i = 0; i < 25; i++) {
-    arr.push
+  for(let i = 0; i < 25; i++) {
+    if(i === 1){
+      arr.push(`I am ${i} strange loop.`);
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+    return arr;
   }
 }
